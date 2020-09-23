500a0ca5b4fed64776bc8680dacc6f88839bd223
410813072 <71751214+410813072@users.noreply.github.com>
2020-09-23 15:39:54 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,13 +8,12 @@ for (let index = 0; index < 4; index++) {
     basic.showNumber(2)
     basic.clearScreen()
     basic.showIcon(IconNames.Butterfly)
+    basic.showString("b")
+    basic.clearScreen()
+    basic.showArrow(ArrowNames.NorthWest)
+    basic.pause(500)
+    music.rest(music.beat(BeatFraction.Whole))
 }
-continue;
-basic.showString("b")
-basic.clearScreen()
-basic.showArrow(ArrowNames.NorthWest)
-basic.pause(500)
-music.rest(music.beat(BeatFraction.Whole))
 basic.forever(function () {
 	
 })
