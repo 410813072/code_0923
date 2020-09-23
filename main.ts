3079fe25297c4ecaf12ef74fb1522ee10957b4a5
410813072 <71751214+410813072@users.noreply.github.com>
2020-09-23 15:36:09 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,11 @@ basic.showString("@")
 basic.pause(100)
 basic.clearScreen()
 basic.showIcon(IconNames.Heart)
+for (let index = 0; index < 4; index++) {
+    basic.showNumber(2)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Butterfly)
+}
 basic.forever(function () {
 	
 })
